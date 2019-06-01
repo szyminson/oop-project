6a6beddd2a0fd09da31494e01934650e9ee8ee40
szyminson <szyminson@hotmail.com>
2019-06-01 20:23:15 +0200

    Added javadoc comments to Vector class / Resolved conflict with master

--- src/main/java/oop/project/Vector.java
+++ src/main/java/oop/project/Vector.java
@@ -1,39 +1,81 @@
 package oop.project;
 
+
 import java.util.Objects;
 
+/**
+ * An implementation of mathematical vector in the project.
+ */
 public class Vector {
     private final double x, y;
 
+    /**
+     * Sets x and y coordinates' of the vector.
+     * @param x Vector coordinate.
+     * @param y Vector coordinate.
+     */
     public Vector(double x, double y) {
         this.x = x;
         this.y = y;
     }
 
+    /**
+     * Gives access to x coordinate from outside of the class.
+     * @return Returns x coordinate of the vector.
+     */
     public double getX() {
         return this.x;
     }
 
+    /**
+     * Gives access to y coordinate from outside of the class.
+     * @return Returns y coordinate of the vector.
+     */
     public double getY() {
         return this.y;
     }
 
+    /**
+     * Adds other vector to this vector.
+     * @param v Other vector object.
+     * @return Sum of two vectors.
+     */
     public Vector add(Vector v) {
         return new Vector(this.x + v.x, this.y + v.y);
     }
 
+    /**
+     * Subtracts other vector from this vector.
+     * @param v Other vector object.
+     * @return Difference between two vectors.
+     */
     public Vector sub(Vector v) {
         return new Vector(this.x - v.x, this.y - v.y);
     }
 
+    /**
+     * Multiplies this vector by given number.
+     * @param d Number.
+     * @return This vector multiplied.
+     */
     public Vector mul(double d) {
         return new Vector(this.x * d, this.y * d);
     }
 
+    /**
+     * Divides this vector by given number.
+     * @param d Number.
+     * @return This vector divided.
+     */
     public Vector div(double d) {
         return new Vector(this.x / d, this.y / d);
     }
 
+    /**
+     * Scalar multiplication of this vector and other vector.
+     * @param v Other vector object.
+     * @return A number, scalar product of multiplication.
+     */
     public double dotProduct(Vector v) {
         return this.x * v.x + this.y * v.y;
     }
