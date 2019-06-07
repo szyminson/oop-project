f6dea8d1dbb2df73c7c3f70e7da5009366096bd8
threeyedbird <49250750+threeyedbird@users.noreply.github.com>
2019-06-07 23:03:40 +0200

    Created comments for World.java

--- src/main/java/oop/project/World.java
+++ src/main/java/oop/project/World.java
@@ -2,10 +2,17 @@ package oop.project;
 
 import java.util.List;
 
+
 public class World {
+    /**
+     *Parameters of this World.
+     */
     private Rocket rocket;
     private List<IForceField> forceFields;
 
+    /**
+     * Runs Simulation.
+     */
     public void runSimulation() {
         throw new UnsupportedOperationException("Not implemented");
     }
