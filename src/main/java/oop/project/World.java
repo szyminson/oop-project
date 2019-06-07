689d2ef805598a5cff3fbe94fcce63f0bc330f9d
Barteks2x <Barteks2x@gmail.com>
2019-06-07 06:00:24 +0200

    Add an object that can update gui every simulation iteration.

--- src/main/java/oop/project/World.java
+++ src/main/java/oop/project/World.java
@@ -8,22 +8,27 @@ public class World {
     private List<IForceField> forceFields;
     private Predicate<Rocket> endCondition;
 
+    private Runnable uiUpdateHandler;
+
     private double timeStep;
     private double time = 0;
 
-    public World(Rocket rocket, List<IForceField> forceFields, Predicate<Rocket> endCondition, double timeStep) {
+    public World(Rocket rocket, List<IForceField> forceFields, Predicate<Rocket> endCondition, double timeStep, Runnable uiUpdateHandler) {
         this.rocket = rocket;
         this.forceFields = forceFields;
         this.endCondition = endCondition;
         this.timeStep = timeStep;
+        this.uiUpdateHandler = uiUpdateHandler;
     }
 
     public void runSimulation() {
         while (!endCondition.test(rocket)) {
+            Vector directionVector = new Vector(Math.cos(rocket.getDirection()), Math.sin(rocket.getDirection()));
             Vector totalForce = forceFields.stream()
-                    .map(f -> f.getForce(rocket.getPosition(), rocket.getDirection(), rocket.getVelocity(), rocket.getMass(), time))
+                    .map(f -> f.getForce(rocket.getPosition(), directionVector, rocket.getVelocity(), rocket.getMass(), time))
                     .reduce(Vector::add).orElseGet(() -> new Vector(0, 0));
             rocket.updateRocket(totalForce, time, timeStep);
+            uiUpdateHandler.run();
             time += timeStep;
         }
     }
