70e6b7aaccae7dbe674a37145c0ad728b3f32131
Barteks2x <Barteks2x@gmail.com>
2019-06-07 03:54:33 +0200

    Additional engine test

--- src/test/java/oop/project/TestEngine.java
+++ src/test/java/oop/project/TestEngine.java
@@ -20,4 +20,15 @@ public class TestEngine {
         assertEquals(1.5, engine.createThrust(direction, 0.5).length(), delta);
         assertEquals(2.5, engine.createThrust(direction, 2).length(), delta);
     }
+
+    @Test
+    public void testEngineThrustSingleValue() {
+        final double delta = 0.0001;
+        Vector direction = new Vector(1, 0); // unit vector
+        Map<Double, Double> thrustMap = new HashMap<>();
+        thrustMap.put(0.0, 1.0);
+        Engine engine = new Engine(1, new FuelContainer(1, Double.MAX_VALUE), thrustMap);
+        assertEquals(1.0, engine.createThrust(direction, 0.5).length(), delta);
+        assertEquals(1.0, engine.createThrust(direction, 2).length(), delta);
+    }
 }
