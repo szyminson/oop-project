dc301b215146ff0c589e4a556f3367c4d7dcacfc
threeyedbird <49250750+threeyedbird@users.noreply.github.com>
2019-06-07 13:12:51 +0200

    Created doc comments for FuelContainer class

--- src/main/java/oop/project/FuelContainer.java
+++ src/main/java/oop/project/FuelContainer.java
@@ -1,26 +1,42 @@
 package oop.project;
 
-
 public class FuelContainer implements IRocketPart {
-    private double containerMass;
+    /**
+     Parameters of this FuelContainer.
+     */
+    private final double containerMass;
     private double fuelMass;
-
+    /**
+     * Sets containerMass and fuelMass of this FuelContainer.
+     * @param containerMass of FuelContainer.
+     * @param fuelMass of FuelContainer.
+     */
+        public FuelContainer(double containerMass, double fuelMass) {
+        this.containerMass = containerMass;
+        this.fuelMass = fuelMass;
+    }
+    /**
+     * Gets RemainingFuel in this FuelContainer.
+     * @return fuelMass of this FuelContainer.
+     */
     public double getRemainingFuel() {
-
         return this.fuelMass;
     }
 
+    /**
+     * Takes given amount of fuel from this FuelContainer.
+     * @param amount Given amount.
+     **/
     public void takeFuel(double amount) {
         this.fuelMass -= amount;
     }
 
+    /**
+     * Gets Mass of this FuelContainer.
+     * @return Combined mass of this FuelContainer.
+     */
     @Override
     public double getMass() {
         return this.containerMass + this.fuelMass;
-    }
-
-    @Override
-    public Vector createThrust(Vector rotation) {
-        return null;
     }
 }
