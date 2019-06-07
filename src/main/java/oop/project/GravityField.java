8d834a119d3b42406e7c228fecfb480f09a45f62
threeyedbird <49250750+threeyedbird@users.noreply.github.com>
2019-06-07 22:52:15 +0200

    Created comments for GravityField.java

--- src/main/java/oop/project/GravityField.java
+++ src/main/java/oop/project/GravityField.java
@@ -2,13 +2,28 @@ package oop.project;
 
 
 public class GravityField implements IForceField {
+    /**
+     *Parameters of this GravityField.
+     */
     private final double strength;
     private final Vector sourcePosition;
-
+    /**
+     * @param sourcePosition of this GravityField.
+     * @param strength of this Gravity Field.
+     */
     public GravityField(Vector sourcePosition, double strength) {
         this.sourcePosition = sourcePosition;
         this.strength = strength;
     }
+    /**
+     * Calculates force of this GravityField using given parameters.
+     * @param position Given position.
+     * @param rotation Given rotation.
+     * @param velocity Given velocity.
+     * @param mass Given mass.
+     * @param time Given time.
+     * @return Calculated force.
+     */
     @Override
     public Vector getForce(Vector position, Vector rotation, Vector velocity, double mass, double time) {
         Vector diff = this.sourcePosition.sub(position);
