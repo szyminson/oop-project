3bd956d37f84d839b5a091470ed528b10874fd16
Barteks2x <Barteks2x@gmail.com>
2019-06-07 04:05:56 +0200

    Initial implementation of World

--- src/main/java/oop/project/World.java
+++ src/main/java/oop/project/World.java
@@ -1,12 +1,30 @@
 package oop.project;
 
 import java.util.List;
+import java.util.function.Predicate;
 
 public class World {
     private Rocket rocket;
     private List<IForceField> forceFields;
+    private Predicate<Rocket> endCondition;
+
+    private double timeStep;
+    private double time = 0;
+
+    public World(Rocket rocket, List<IForceField> forceFields, Predicate<Rocket> endCondition, double timeStep) {
+        this.rocket = rocket;
+        this.forceFields = forceFields;
+        this.endCondition = endCondition;
+        this.timeStep = timeStep;
+    }
 
     public void runSimulation() {
-        throw new UnsupportedOperationException("Not implemented");
+        while (!endCondition.test(rocket)) {
+            Vector totalForce = forceFields.stream()
+                    .map(f -> f.getForce(rocket.getPosition(), rocket.getDirection(), rocket.getVelocity(), rocket.getMass(), time))
+                    .reduce(Vector::add).orElseGet(() -> new Vector(0, 0));
+            rocket.updateRocket(totalForce, time, timeStep);
+            time += timeStep;
+        }
     }
 }
