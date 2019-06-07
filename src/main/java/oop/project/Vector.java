20383709d9be570f802309dc036e5ff5e17c44db
Barteks2x <Barteks2x@gmail.com>
2019-06-07 03:55:12 +0200

    Fix Vector angle calculation. Math.atan2 has x and y in reverse order.

--- src/main/java/oop/project/Vector.java
+++ src/main/java/oop/project/Vector.java
@@ -105,7 +105,7 @@ public class Vector {
      * @return Angle of this vector.
      */
     public double getAngle() {
-        return Math.atan2(this.x, this.y);
+        return Math.atan2(this.y, this.x);
     }
 
     /**
